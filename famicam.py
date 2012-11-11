f150553396dbf233f135d377f43e071293e4da1f
thantik <thantik@gmail.com>
2012-11-11 01:23:43 -0500

    Cleared out cobwebs, starting from base

--- famicam.py
+++ famicam.py
@@ -1,26 +1,18 @@
-import cv
 import sys
 import urllib
 import time
 import os
-import pycurl
-import twitter
-import imgur
-from pygame.transform import scale
-from pygame import image
+from PIL import Image
+from StringIO import StringIO
 
 # Settings
-camera_ip = ""
-haar_file = "" # Ex: "C:\OpenCV2.0\data\haarcascades\haarcascade_frontalface_default.xml"
+camera_ip = "localhost:8080"
 twitter_user = ""
 twitter_pass = ""
 imgur_api_key = ""
 WIDTH = 640
 HEIGHT = 480
 
-# Common Objects
-hc = cv.Load(haar_file)
-laughingman = image.load('laughing_man.png')
 stamp = time.strftime("%y%d%m%H%M%S",time.localtime())
 
 # Loop over the views
@@ -33,11 +25,17 @@ for index, v in enumerate(views):
     
     # Collect image (color, and black and white versions)
     source = os.path.join('./', stamp + '.jpg') 
-    data = urllib.urlretrieve("http://"+camera_ip+"/oneshotimage.jpg", source)
+    data = StringIO(urllib.urlopen("http://"+camera_ip+"/oneshotimage.jpg").read())
+
+    tempimage = Image.open(data)
+    tempimage.show()
+"""
     bwimg = cv.LoadImage(source, 0)
     img = cv.LoadImage(source)
+"""
 
-    # Create pygame surfaces for bitblitting
+"""
+# Create pygame surfaces for bitblitting
     pygameimg = img.tostring()
     with_laughingman = image.fromstring(pygameimg, (WIDTH,HEIGHT), "RGB")
     
@@ -55,20 +53,8 @@ for index, v in enumerate(views):
 
     print tweet	
 
-
-    # Check if the room is dark
-    hist = cv.CreateHist([32], cv.CV_HIST_ARRAY, [[0, 255]], 1)
-
-    cv.CalcHist([bwimg], hist, 0, None)
-    hist_img = cv.CreateImage((32*10,255), 8, 3)
-    (_,max_value,_,_) = cv.GetMinMaxHistValue(hist)
-
-    (sizex, sizey) = cv.GetSize(bwimg)
-
-    darkcount = 0
-    for i in range(8):
-       darkcount = darkcount + cv.QueryHistValue_1D(hist, i)
-
+"""
+"""
     if darkcount > (.7 * (sizex * sizey)):
         print "Image is dark. Not sending data."
     else:
@@ -84,4 +70,5 @@ for index, v in enumerate(views):
 		        print "Failed!"
 				
     # Clean up our mess
-    os.remove(source)
+"""
+    #os.remove(source)
